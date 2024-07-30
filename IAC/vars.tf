6f8135e467759fbf5f99d1c8a887c9ec9a5abb9a
satuluriakhil420 <94843341+satuluriakhil420@users.noreply.github.com>
2024-07-30 12:22:14 +0530

    Update vars.tf

--- IAC/vars.tf
+++ IAC/vars.tf
@@ -5,12 +5,7 @@ variable "environment" {
 
 locals {
   glue_job_script_locations = [
-    "ens360-dashboard-c1-${var.environment}-01",
-    "ens360-dashboard-loc-${var.environment}-01", 
-    "ens360-dashboard-oc-${var.environment}-01",
-    "ens360-dashboard-pc-${var.environment}-01",
-    "ens360-dashboard-pg-${var.environment}-01",
-    "ens360-dashboard-sl-${var.environment}-01"
+    "ent360-dashboard-sl-${var.environment}-01"
   ]
 }
 
